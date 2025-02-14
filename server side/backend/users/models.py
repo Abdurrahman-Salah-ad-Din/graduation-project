230376081ce504f7fcbb9677c8febabcdae0a8b3
Abdurrahman <abdosalahas.01128@gmail.com>
2025-02-14 23:02:55 +0200

    clean up code

--- server side/backend/users/models.py
+++ server side/backend/users/models.py
@@ -4,16 +4,23 @@ from django.contrib.auth.models import AbstractUser, UserManager
 class RadiologistManager(UserManager):
     def create_user(self, email, password = None, **extra_fields):
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError('The Email field must be set.')
+        
         email = self.normalize_email(email=email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-    def create_superuser(self, email, password, **extra_fields):
 
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
         return self.create_user(email, password, **extra_fields)
 
 class Radiologist(AbstractUser):
@@ -22,26 +29,20 @@ class Radiologist(AbstractUser):
         NURSE = 'N', 'Nurse'
 
     class GenderChoices(models.TextChoices):
-        male = 'M', 'Male'
-        Female = 'F', 'Female'
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
 
-    date_of_birth = models.DateField(null=True, blank=True)
-    job = models.CharField(max_length=1,choices=JobChoices)
-    gender = models.CharField(max_length=1, choices=GenderChoices)
+    date_of_birth = models.DateField()
+    job = models.CharField(max_length=1,choices=JobChoices.choices)
+    gender = models.CharField(max_length=1, choices=GenderChoices.choices)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=12, unique=True, blank=True, null=True)
     username = None
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('first_name', 'last_name', 'job', 'gender')
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'job', 'gender', 'phone_number', 'date_of_birth']
 
     objects = RadiologistManager()
 
     def __str__(self):
         return f"{self.get_full_name()}"
-    
-    def has_perm(self, perm, obj = None):
-        return self.is_staff
-    
-    def has_module_perms(self, app_label):
-        return True
