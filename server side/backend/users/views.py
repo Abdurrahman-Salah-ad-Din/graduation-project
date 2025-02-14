948fae06d527eae4f4456d0d80bcd5c280ca4235
Abdurrahman <abdosalahas.01128@gmail.com>
2025-02-14 23:13:30 +0200

    update `RadiologistViewSet`

--- server side/backend/users/views.py
+++ server side/backend/users/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, permissions
 from .models import Radiologist
-from .serializers import RadiologistRegisterSerializer
+from .serializers import RadiologistSerializer
 
 class RadiologistViewSet(viewsets.ModelViewSet):
     queryset = Radiologist.objects.all()
-    serializer_class = RadiologistRegisterSerializer
+    serializer_class = RadiologistSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        radiologists = super().get_queryset()
+        if not self.request.user.is_staff:
+            radiologists = radiologists.filter(id=self.request.user.id)
+        return radiologists
