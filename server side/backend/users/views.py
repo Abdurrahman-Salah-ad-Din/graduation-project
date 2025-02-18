9695e6c9b061d0501dee25953907d9bf670690d2
Abdurrahman <abdosalahas.01128@gmail.com>
2025-02-18 23:08:28 +0200

    create `LoginView`

--- server side/backend/users/views.py
+++ server side/backend/users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Radiologist
-from .serializers import RadiologistSerializer
+from .serializers import RadiologistSerializer, LoginSerializer
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 class RadiologistViewSet(viewsets.ModelViewSet):
     queryset = Radiologist.objects.all()
@@ -13,3 +17,6 @@ class RadiologistViewSet(viewsets.ModelViewSet):
         if not self.request.user.is_staff:
             radiologists = radiologists.filter(id=self.request.user.id)
         return radiologists
+    
+class LoginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
