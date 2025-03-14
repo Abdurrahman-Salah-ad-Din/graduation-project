54a7b6f97502f23e17c851692eec673fbefe72b9
Abdurrahman <abdosalahas.01128@gmail.com>
2025-03-14 14:08:29 +0200

    update models

--- server side/backend/patients/models.py
+++ server side/backend/patients/models.py
@@ -1,12 +1,10 @@
-from tkinter import CASCADE
 import uuid
-from django.utils import timezone
 from django.db import models
 from users.models import Radiologist
 from core.utils import get_secret_code
 
 class Patient(models.Model):
-    class GenderChoices:
+    class GenderChoices(models.TextChoices):
         MALE = 'M', 'Male'
         FEMALE = 'F', 'Female'
 
@@ -15,24 +13,27 @@ class Patient(models.Model):
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     email = models.EmailField()
+    code = models.CharField(max_length=6, default=generate_random_code)
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=1, choices=GenderChoices)
-    code = models.CharField(max_length=6, default=generate_random_code)
     created_by = models.ForeignKey(Radiologist, on_delete=models.SET_NULL, null=True, blank=True, related_name="created_patients")
     radiologists = models.ManyToManyField(Radiologist, through='PatientRadiologistAccess', related_name='accessible_patients')
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.first_name} {self.last_name} diagnosed by {self.diagnosed_by}"
     
 class PatientRadiologistAccess(models.Model):
-    radiologist = models.ForeignKey(Radiologist, on_delete=CASCADE)
-    patient = models.ForeignKey(Patient, on_delete=CASCADE)
+    radiologist = models.ForeignKey(Radiologist, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     granted_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ('patient', 'radiologist')
+        indexes = [
+            models.Index(fields=['patient', 'radiologist'])
+        ]
     
     def __str__(self):
         return f"Access for {self.radiologist.get_full_name()} to patient {self.patient.first_name} {self.patient.last_name} granted at {self.granted_at}"
