e71f38cfa611f974c3a592a57257eb5364b2f0e4
Abdurrahman <abdosalahas.01128@gmail.com>
2025-03-14 14:14:50 +0200

    creaet patients views

--- server side/backend/patients/views.py
+++ server side/backend/patients/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework import views, viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .serializers import PatientAccessRequestSerialzier, PatientSerializer
+from .models import Patient
 
-# Create your views here.
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.select_related('created_by').prefetch_related("radiologists")
+    serializer_class = PatientSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return super().get_queryset().filter(Q(created_by=user) | Q(radiologists=user)).distinct()
+
+    def get_serializer_class(self):
+        if self.action == "grant_access":
+            return PatientAccessRequestSerialzier
+        return super().get_serializer_class()
+
+    @action(detail=False, methods=["post"], url_path="grant-access",)
+    def grant_access(self, request):
+        serializer = PatientAccessRequestSerialzier(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"detail": "Access granted."}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
