581a550eab664b012886a011eba7c79b81eee49d
Abdurrahman <abdosalahas.01128@gmail.com>
2025-03-16 23:35:17 +0200

    create view

--- server side/backend/scans/views.py
+++ server side/backend/scans/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .models import PatientScan
+from rest_framework import viewsets, permissions
+from .serializers import PatientScanSerializer
 
-# Create your views here.
+class PatientScanView(viewsets.ModelViewSet):
+    queryset = PatientScan.objects.select_related('patient').prefetch_related('predictions').all()
+    serializer_class = PatientScanSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return self.queryset.filter(patient__radiologists=user).distinct()
