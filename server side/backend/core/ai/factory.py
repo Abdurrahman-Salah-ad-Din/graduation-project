e44dac6c8a36c1be595aafd2c885a02ad2c8a75b
Abdurrahman <abdosalahas.01128@gmail.com>
2025-03-21 15:10:24 +0200

    clean up code

--- server side/backend/core/ai/factory.py
+++ server side/backend/core/ai/factory.py
@@ -6,6 +6,7 @@ def get_ai_model(organ):
     if organ == OrganChoices.CHEST:
         model_path = settings.CHEST_MODEL_PATH
         labels = settings.CHEST_MODEL_LABELS
-        return TFClassificationModel(model_path=model_path, target_size=(244, 244), labels=labels)
+        target_size = settings.CHEST_MODEL_TARGET_SIZE
+        return TFClassificationModel(model_path=model_path, target_size=target_size, labels=labels)
     else:
         raise ValueError("Invalid organ.")
