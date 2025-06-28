f6ef6159bf74d8bf418eeb7f9b3718562506d6b6
Abdurrahman <abdosalahas.01128@gmail.com>
2025-06-28 14:27:44 +0300

    update `PatientScanView`

--- server side/backend/scans/views.py
+++ server side/backend/scans/views.py
@@ -7,6 +7,7 @@ from django.db import transaction
 from django.db.models import Prefetch
 from core.exceptions import AppException
 from core.errors import ErrorCodes
+from rest_framework.parsers import MultiPartParser, FormParser
 
 class PatientScanView(mixins.ListModelMixin,
                       mixins.RetrieveModelMixin,
@@ -16,6 +17,7 @@ class PatientScanView(mixins.ListModelMixin,
     queryset = PatientScan.objects.select_related('patient').prefetch_related(Prefetch('predictions', queryset=ScanDiseasePrediction.objects.select_related('disease')), 'predictions__disease').all()
     serializer_class = PatientScanSerializer
     permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
 
     def get_queryset(self):
         user = self.request.user
@@ -30,7 +32,7 @@ class PatientScanView(mixins.ListModelMixin,
     
         with transaction.atomic():        
             scan = serializer.save()
-            predictions_data = get_ai_model(OrganChoices(scan.organ)).predict(scan.image_scan_url.path)
+            predictions_data = get_ai_model(OrganChoices(scan.organ)).run(scan.image_scan_url.path)
             predicted_disease_names = list(predictions_data.keys())
             existing_diseases = Disease.objects.filter(name__in=predicted_disease_names)
             disease_map = {d.name: d for d in existing_diseases}
